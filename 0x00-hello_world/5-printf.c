675e15bf3c864ff5237f5298ab9efb99b0e91ef2
raim <raim.tobi@gmail.com>
2023-06-15 15:35:44 -0700

    added a 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main: program entrypoint.
+ *
+ * Descriptioion: prints a line of multiple strings, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		
+	return (0);
+}
