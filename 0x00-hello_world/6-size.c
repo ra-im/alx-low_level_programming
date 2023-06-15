28d6eb1a1a8953b550e41a09fd51ed626dfdee44
raim <raim.tobi@gmail.com>
2023-06-15 16:27:15 -0700

    updated 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: prints the size of various types 
- * 		on the computer it is compiled and run on.
+ * Description: prints the size of various types
+ *		on the computer it is compiled and run on.
  *
  * Return: Always 0 (succesful)
  */
