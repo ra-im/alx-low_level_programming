80dffdac05839e0ad97097afbd2923e73d1f7297
raim <raim.tobi@gmail.com>
2023-06-16 05:16:26 -0700

    added a 6-print_numberz script

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers
+ *		of base 10, starting from 0,
+ *		followed by a new line.
+ *
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for(numbers = 0; numbers <= 9, numbers++)
+	{
+		putchar('0' + numbers);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
