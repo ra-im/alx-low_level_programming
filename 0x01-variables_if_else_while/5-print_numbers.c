8116c27427275702f422eec220954eb925155a5f
raim <raim.tobi@gmail.com>
2023-06-16 05:08:23 -0700

    added a 5-print_numbers.c script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#incude ,stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers
+ *		of base 10, starting from 0,
+ *		folowed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0;  numbers <= 9; numbers++)
+	{
+		putchar('0' + numbers);
+	}
+
+	putchar('\n');
+
+	Return (0);
+}
+
