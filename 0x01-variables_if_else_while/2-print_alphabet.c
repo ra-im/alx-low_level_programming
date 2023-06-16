a00ed6598c3d549d928309f569ed1d95c019d135
raim <raim.tobi@gmail.com>
2023-06-16 04:23:47 -0700

    added a 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lower case
+ *		followed by a new line.
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char = letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
