b688f424d0d6915c237863513c020cebd36f2026
raim <raim.tobi@gmail.com>
2023-06-15 17:23:15 -0700

    ...

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,18 +5,18 @@
  * main - Entry point
  *
  * Description: prints exactly the string
- *		` piece of art is useful"
- *		- Dora Korpar, 2015-10-19,
+ *		`and that piece of art is useful"
+ *		- Dora Korpar, 2015-10-19`,
  *		followed by a new line,
- *		to the standard error.`
+ *		to the standard error.
  *
  * Return: Always 0 (successful)
  */
 
 int main()
 {
-	const string message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, message, sizeof(message)-1);
-	return 1;
+	return (1);
 }
 
