1b1eb818dc0bd26601242136fde28f456bbe8c08
raim <raim.tobi@gmail.com>
2023-06-16 07:44:27 -0700

    added a 9-print_comb.c script

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all the possible combinations
+ *		of single-digit number.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int number;
+
+	putchar('0');
+
+	for (number = 1; number <= 9, number++)
+	{
+		putchar(', ');
+
+		putchar('0' + number);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
