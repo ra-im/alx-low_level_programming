71a10f7dbb3cddde48610ef4f89310b4d40003ee
raim <raim.tobi@gmail.com>
2023-06-20 13:58:59 -0700

    added 0-putchar.c script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Printss '_putchar',
+ *		followed by a new line.
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
