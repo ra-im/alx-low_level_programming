887229c16c97fb0819aa6d4176b14445263a5494
raim <raim.tobi@gmail.com>
2023-06-20 15:41:16 -0700

    added 3-islower script

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: Checks for lowercase character.
+ *
+ * Return: Always 0 (success).
+ */
+
+int _islower(int c)
+{
+	return (c <= 'z' && c >= 'a')
+}
