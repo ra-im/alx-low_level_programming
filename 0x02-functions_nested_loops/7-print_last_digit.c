5650a21d79a5c7eaf98b9b44780d0e687340ffc3
raim <raim.tobi@gmail.com>
2023-06-20 16:43:49 -0700

    added 7-print_last_digit script

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @number: Number whose last digit is to be printed.
+ *
+ * Description: Checks for lowercase character.
+ *
+ * Return: number % 10 (success).
+ */
+
+int print_last_digit(int number)
+{
+	return (number % 10);
+}
