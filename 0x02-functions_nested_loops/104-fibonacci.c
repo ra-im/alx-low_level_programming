0a2ae6fae87a8ba630208354b8efeaaa9e9f9d76
raim <raim.tobi@gmail.com>
2023-06-20 21:08:41 -0700

    added 104-fibonacci script

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,25 +12,24 @@
 
 int main(void)
 {
-	int i, fib_1, fib_2;
-	unsigned long int j1, j2, k1, k2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	fib_1 = 1;
-	fib_2 = 2;
+	j = 1;
+	k = 2;
 
-	printf("%d", fib_1);
+	printf("%lu", j);
 
 	for (i = 1; i < 91; i++)
 	{
-		printf(", %d", fib_2);
-		fib_2 = fib_2 + fib_1;
-		fib_1 = fib_2 - fib_1;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
 
-	j1 = fib_1 / 1000000000;
-	j2 = fib_1 % 1000000000;
-	k1 = fib_2 / 1000000000;
-	k2 = fib_2 % 1000000000;
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
 
 	for (i = 92; i < 99; ++i)
 	{
