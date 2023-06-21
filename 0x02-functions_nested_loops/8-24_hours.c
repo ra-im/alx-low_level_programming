dd20ed27e1c6b7c82b418361be82e1b9bc8d70bb
raim <raim.tobi@gmail.com>
2023-06-20 17:33:47 -0700

    added 8-24_hours.c script

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: Prints every minute of the day
+ *		of Jack Bauer,
+ *		starting from 00:00 to 23:59
+ *
+ * Return: Always void (success).
+ */
+
+void jack_bauer(void)
+{
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar((':'));
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+		}
+}
