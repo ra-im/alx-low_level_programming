f09a00958f08dd325fccce9b2ecc4c6b6cce1b5d
raim <raim.tobi@gmail.com>
2023-06-20 19:41:00 -0700

    added 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the first 50 Fibonacci numbers,
+ *		starting with 1 and 2,
+ *		followed by a new line.
+ *
+ * Return: Always 0 (success).
+ */
+
+void fibonacci_numbers(int n) {
+    int fibonacci_arr[50];
+    fibonacci_arr[0] = 1;  /* First Fibonacci number*/
+    fibonacci_arr[1] = 2;  /* Second Fibonacci number*/
+
+    /* Generate Fibonacci sequence up to n numbers*/
+    for (int i = 2; i < n; i++) {
+        fibonacci_arr[i] = fibonacci_arr[i - 1] + fibonacci_arr[i - 2];
+    }
+
+    /* Print the sequence with comma-separated values*/
+    for (int i = 0; i < n; i++) {
+        printf("%d", fibonacci_arr[i]);
+
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+
+    printf("\n");
+}
+
+int main() {
+    /* Print the first 50 Fibonacci numbers*/
+    fibonacci_numbers(50);
+
+    return 0;
+}
+	
