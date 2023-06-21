b0af9bb74432c21cda5a3c08e935c005bbe9ad28
raim <raim.tobi@gmail.com>
2023-06-20 19:54:30 -0700

    updated 102-fibonacci.c script

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,37 +10,27 @@
  * Return: Always 0 (success).
  */
 
-void fibonacci_numbers(int n)
-{
-	int i;
-
-	int fibonacci_arr[50];
-	fibonacci_arr[0] = 1;  /* First Fibonacci number*/
-	fibonacci_arr[1] = 2;  /* Second Fibonacci number*/
-
-    /* Generate Fibonacci sequence up to n numbers*/
-	for (i = 2; i < n; i++)
-	{
-		fibonacci_arr[i] = fibonacci_arr[i - 1] + fibonacci_arr[i - 2];
-	}
-       
-	/* Print the sequence with comma-separated values*/
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", fibonacci_arr[i]);
-		if (i < n - 1)
-		{
-			printf(", ");
-		}
-	}
-	
-	printf("\n");
-}
-
 int main(void)
 {
-	/* Print the first 50 Fibonacci numbers*/
-	fibonacci_numbers(50);
+	long int i, first_fib_num, second_fib_num, next_fib_num;
+
+	first_fib_num = 1;
+
+	second_fib_num = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (first_fib_num != 20365011074)
+		{
+			printf("%ld, ", first_fib_num);
+		} else
+		{
+			printf("%ld\n", first_fib_num);
+		}
+		next_fib_num = first_fib_num + second_fib_num;
+		first_fib_num = second_fib_num;
+		second_fib_num = next_fib_num;
+	}
 	
 	return 0;
 }
