6984070946f4fb0289721d3e735b5fdb3575680b
raim <raim.tobi@gmail.com>
2023-06-20 18:02:04 -0700

    added 10-add.c script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @integer_1: one of the integers to be added.
+ * @integer_2: one of the inegers to be added.
+ *
+ * Description: adds two integers and return the result.
+ *
+ * Return: Always result (success).
+ */
+
+int add(int integer_1, int integer_2)
+{
+	int result;
+
+	result = integer_1 + integer_2;
+
+	return (result);
+}
