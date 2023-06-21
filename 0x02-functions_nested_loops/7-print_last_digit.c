0472b57a29d1e02ee385620b5be4ab450043af87
raim <raim.tobi@gmail.com>
2023-06-20 17:05:16 -0700

    updated 7-print_last_digit script

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -19,8 +19,11 @@ int print_last_digit(int number)
 		number = -number;
 
 	last_digit = number % 10;
+	
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
-	_putchar(last_digit);
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
