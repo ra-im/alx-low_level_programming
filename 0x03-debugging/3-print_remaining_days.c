7d7bf794a9739acd07dda5f4136cbec31c1147bc
raim <raim.tobi@gmail.com>
2023-06-22 14:29:42 -0700

    updated 3-print_remaining_days.c script

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,10 +18,8 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	/* reset the condition of if-statement for a leap year */
 	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
-		/* if-condition for months beyond february */
 		if (month > 2)
 			day++;
 		printf("Day of the year: %d\n", day);
@@ -29,13 +27,10 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		/* if-condition for the month of february and day 60 */
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n",
-					month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-		/* condition for every other month */
 		else
 		{
 			printf("Day of the year: %d\n", day);
