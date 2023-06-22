09c92e9f3093f7aca717d1414bf9cf10a5895b73
raim <raim.tobi@gmail.com>
2023-06-22 15:55:30 -0700

    created main.h file

--- 0x04-more_functions_nested_loops/_isupper.c
+++ 0x04-more_functions_nested_loops/_isupper.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * _isupper - Checks for uppercase characters.
  * @c: Character to be checked.
- * Return: "1", if c is uppercase; "0" otherwise.
+ * Return: (1), if c is uppercase; (0) otherwise.
  */
 
 int _isupper(int c)
