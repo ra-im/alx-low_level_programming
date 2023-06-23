7588628c8640b529c6760bacb6f7fcbd02c56982
raim <raim.tobi@gmail.com>
2023-06-22 18:09:56 -0700

    added 3-print_numbers sscript

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9,
+ * followed by a new line.
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar(i + '0');
+	}
+
+	_puchaar('\n');
+}
