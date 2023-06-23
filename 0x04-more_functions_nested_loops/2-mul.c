521fa4823bdda193fbde13aae776169b97858280
raim <raim.tobi@gmail.com>
2023-06-22 18:02:34 -0700

    added 2-mul.c script

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers.
+ * @a: one of multiples
+ * @b: one of the multiples
+ * Return: result (success).
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
