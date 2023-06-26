9dac2006cb7b758f02efe35a677d9606c3da2bf2
raim <raim.tobi@gmail.com>
2023-06-26 09:00:36 -0700

    added reset_to_98.c script

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an in as parameter
+ *		and updates the value it points to to 98.
+ *
+ * @n: integer parameter to be updated.
+ *
+ * Return: void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
