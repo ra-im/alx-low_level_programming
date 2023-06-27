a229abe76fdface6267a976e4cc29c70e327f27c
raim <raim.tobi@gmail.com>
2023-06-26 17:30:11 -0700

    updated 5-rev_string.c script

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,6 +11,7 @@
 void rev_string(char *s)
 {
 	int  counter_1, counter_2, string_length;
+	char tmp;
 
 	counter_1 = 0;
 
@@ -19,13 +20,23 @@ void rev_string(char *s)
 		counter_1++;
 	}
 
-	string_length = counter_1;
+	string_length = counter_1 - 1;
 
+	/*
 	for (counter_2 = string_length - 1;
 			counter_2 >= 0;
 			counter_2--)
 	{
 		_putchar(s[counter_2]);
+	}
+	*/
+
+	for (counter_2 = 0; counter_2 < string_length / 2;
+			counter_2++)
+	{
+		tmp = s[counter_2];
+		s[counter_2] = s[string_length];
+		s[string_length--] = tmp;
 	}
 }
 
