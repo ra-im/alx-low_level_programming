545175faf31db7afee3f3276f070dbaf85201ee7
raim <raim.tobi@gmail.com>
2023-06-29 15:51:42 -0700

    added 6-cap_string.c script

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include  <otype.h>
+
+/**
+ * cap_string - Capitalizes all the word of a string.
+ *
+ * @strn: String to be capitalized.
+ *
+ * Return: strn (capitalized character string)
+ */
+
+char *cap_string(char *strn)
+{
+	int counter;
+
+	for (counter = 0;
+			strn[counter] != '\0';
+			counter++)
+	{
+		strn[counter] = toupper(strn[counter]);
+	}
+
+	return (strn);
+}
