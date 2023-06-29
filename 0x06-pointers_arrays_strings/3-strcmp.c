e243f8668a707f8f5b68899dd37ef98992ccd4e8
raim <raim.tobi@gmail.com>
2023-06-29 15:12:17 -0700

    updated  2-strncpy.c script

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,9 +17,9 @@ int _strcmp(char *s1, char *s2)
 			s1[counter] != '\0' && s2[counter] != '\0';
 			counter++)
 	{
-		if (s1[counter != s2[counter])
+		if (s1[counter] != s2[counter])
 		{
-			return (s1[counter - s2[counter]);
+			return (s1[counter] - s2[counter]);
 		}
 	}
 
