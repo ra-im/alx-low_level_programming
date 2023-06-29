ab10abf55c34a1db76a1424ef8a87b0d0ff6486a
raim <raim.tobi@gmail.com>
2023-06-29 16:42:04 -0700

    updaed 6-cap_string.c script

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,14 +11,49 @@
 
 char *cap_string(char *strn)
 {
-	int counter;
+	int counter_1, counter_2;
+	int is_capital;
+	char specs[] = ",;.!?(){}\n\t\" ";
 
-	for (counter = 0;
-			strn[counter] != '\0';
-			counter++)
+	for ( counter_1 = 0, is_capital = 0;
+			strn[counter_1] != '\0';
+			counter_1++)
 	{
-		strn[counter] = toupper(strn[counter]);
-	}
+		if (strn[0] > 96 && strn[0] < 123)
+		{
+			is_capital = 1;
+		}
 
+		for (counter_2 = 0;
+				specs[counter_2] != '\0';
+				counter_2++)
+		{
+			if (specs[counter_2] == strn[counter_1])
+			{
+				is_capital = 1;
+			}
+		}
+
+		if (is_capital)
+		{
+			if (strn[counter_1] > 96 &&
+					strn[counter_1] < 123)
+			{
+				strn[counter_1] -= 32;
+				is_capital = 0;
+			}
+			else if (strn[counter_1] > 64 &&
+					strn[counter_1] < 91)
+			{
+				is_capital = 0;
+			}
+			else if (strn[counter_1] > 47 &&
+					strn[counter_1] < 58)
+			{
+				is_capital = 0;
+			}
+		}
+	}
+	
 	return (strn);
 }
