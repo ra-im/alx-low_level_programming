7db10fe4369e5b36a2882e99aab1958062fe2063
raim <raim.tobi@gmail.com>
2023-06-29 19:43:47 -0700

    updated 6-cap_string.c script

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,50 +8,32 @@
 
 char *cap_string(char *strn)
 {
-	int counter_1, counter_2;
-	int is_capital;
+	int i, j;
+	int trigger;
 	char specs[] = ",;.!?(){}\n\t\" ";
 
-	for (counter_1 = 0, is_capital = 0;
-			strn[counter_1] != '\0';
-			counter_1++)
+	for (i = 0, trigger = 0; strn[i] != '\0'; i++)
 	{
 		if (strn[0] > 96 && strn[0] < 123)
+			trigger = 1;
+		for (j = 0; specs[c] != '\0'; j++)
 		{
-			is_capital = 1;
+			if (specs[j] == strn[i])
+				trigger = 1;
 		}
 
-		for (counter_2 = 0;
-				specs[counter_2] != '\0';
-				counter_2++)
+		if (trigger)
 		{
-			if (specs[counter_2] == strn[counter_1])
+			if (strn[i] > 96 && strn[i] < 123)
 			{
-				is_capital = 1;
+				strn[i] -= 32;
+				trigger = 0;
 			}
-		}
-
-		if (is_capital)
-		{
-			if (strn[counter_1] > 96 &&
-					strn[counter_1] < 123)
-			{
-				strn[counter_1] -= 32;
-				is_capital = 0;
-			}
-			else if (strn[counter_1] > 64 &&
-					strn[counter_1] < 91)
-			{
-				is_capital = 0;
-			}
-			else if (strn[counter_1] > 47 &&
-					strn[counter_1] < 58)
-			{
-				is_capital = 0;
-			}
+			else if (strn[i] > 64 && strn[i] < 91)
+				trigger = 0;
+			else if (strn[i] > 47 && strn[i] < 58)
+				trigger = 0;
 		}
 	}
-	
 	return (strn);
 }
-
