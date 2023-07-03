257cf7e8f01a3ed0f572a205c6a213c24f3772a1
raim <raim.tobi@gmail.com>
2023-07-03 16:49:29 -0700

    added 8-print_diagsums.c script

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Prints the suim of two diagonals
+ * of a squarematrix of integers.
+ *
+ * @a: square matrix of integers.
+ * @size: size of the square matrix.
+ *
+ * Return: void.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int counter;
+	unsigned int sum_1, sum_2;
+
+	sum_1 = 0;
+	sum_2 = 0;
+
+	for (counter = 0;
+			counter < size;
+			counter++)
+	{
+		sum_1 = sum_1 + a[(size * i) + i];
+		sum_2 = sum_2 + a[(size * (i +1)) - (i + 1)];
+	}
+
+	printf('%d, %d\n', sum_1, sum_2)
+}
+
