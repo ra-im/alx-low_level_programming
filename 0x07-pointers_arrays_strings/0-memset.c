844f2b41db8b5a3b5717469dadf1d4c5b0dacb60
raim <raim.tobi@gmail.com>
2023-07-03 07:29:22 -0700

    added memset.c script

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset: Fils the first 'n' bytes of the memory area pointed to by 's',
+ *		with the constant byte 'b'.
+ *
+ * @s: Ponter tothe memory area.
+ * @b: constant byte.
+ * @n: number of the memory areato be filled.
+ *
+ * Return: s (pointer to the memoryarea.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		s[counter] = b;
+	}
+
+	return (s);
+}
+
