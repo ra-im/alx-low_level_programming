186bed4053ce78c9ab638e118e8e15dfcda0c012
raim <raim.tobi@gmail.com>
2023-07-03 17:43:22 -0700

    added 100-set_string.c script

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to a char.
+ *
+ * @s: String to be considered.
+ * @to: String to be changed.
+ *
+ * Return: void.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
