db26a74235780810b32ef0e57156a8e7ee6f7b1a
raim <raim.tobi@gmail.com>
2023-07-03 17:10:48 -0700

    updated 8-print_diagsums.c script

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -23,8 +23,8 @@ void print_diagsums(int *a, int size)
 			counter < size;
 			counter++)
 	{
-		sum_1 = sum_1 + a[(size * i) + i];
-		sum_2 = sum_2 + a[(size * (i +1)) - (i + 1)];
+		sum_1 += a[(size * i) + i];
+		sum_2 += a[(size * (i +1)) - (i + 1)];
 	}
 
 	printf("%d, %d\n", sum_1, sum_2);
