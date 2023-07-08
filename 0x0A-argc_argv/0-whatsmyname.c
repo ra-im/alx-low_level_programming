719d604d42f148e57c4ddc0017cac41dcd45b3c2
raim <raim.tobi@gmail.com>
2023-07-07 17:16:55 -0700

    added 'main.h' eader file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** main - Prints its name, followed by a new line.
+ *
+ * @argc:: number of arguments.
+ * @argb: array of arguments.
+ *
+ * Return: always 0 (successs)
+ */
+
+int main(int argc, char argv[])
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+char _putchar(char *c);
+
+#endif
