d224f45f3847ae6cc887dc2cd4c6fc76bffb71eb
raim <raim.tobi@gmail.com>
2023-07-07 17:45:06 -0700

    added 1-args.c script

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argumets pssed into it.
+ *
+ * @argc: number of arguments supplied.
+ * @argv: array of arguments.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, int *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
