aa8ff8686f4f6cf669abdebae552fdac6a6f0eed
raim <raim.tobi@gmail.com>
2023-07-13 14:35:22 -0700

    added 0-malloc_checked.c script

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mainh"
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ *
+ * @b: Memory to be allocated.
+ *
+ * Return: void.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+
+	if (pointer == NULL)
+		exit(98);
+
+	return (pointer);
+}
+
