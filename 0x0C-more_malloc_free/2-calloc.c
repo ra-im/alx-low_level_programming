b02cf0e413628311727b292a5084325336fff8eb
raim <raim.tobi@gmail.com>
2023-07-13 15:42:32 -0700

    added 2-calloc.c script

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Allocats memory for an array, using malloc.
+ *
+ * @nmemb: number of array elements.
+ * @size: size of each array element, in bytes.
+ *
+ * Return: void.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *pointer;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	pointer = malloc(size * nmemb);
+
+	if (pointer == NULL)
+		return (NULL);
+
+	_memset(pointer, 0, nmemb * size);
+
+	return (pointer);
+}
+
+/**
+ * _memset - fills the allocated memory with constant bytes
+ *
+ * @mem: address to be filled
+ * @str: character string to be copied)
+ * @num: number of times str is to be copied
+ *
+ * Return: pointer to mem.
+ */
+
+char *_memset(char *mem, char *str, unsigned int num)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+		mem[i] = str;
+
+	return (str);
+}
+
