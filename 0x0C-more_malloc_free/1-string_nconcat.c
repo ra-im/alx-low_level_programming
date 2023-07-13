34c34db60af12ee411fe08c0397d70e12d2c8338
raim <raim.tobi@gmail.com>
2023-07-13 15:27:55 -0700

    updaed 1-string_nconcat.c script

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,32 +15,32 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int length_1 = 0, length_2 = 0, i = 0, j = 0;
 	char *new_str;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[length_1])
-		length_1++;
-	while (s2 && s2[length_2])
-		length_2++;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (n < length_2)
-		new_str = malloc(sizeof(char) * (length_1 + n + 1));
+	if (n < len2)
+		new_str = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		new_str = malloc(sizeof(char) * (length_1 + length_2 + 1));
+		new_str = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (!new_str)
 		return (NULL);
 
-	while (i < length_1)
+	while (i < len1)
 	{
 		new_str[i] = s1[i];
 		i++;
 	}
 
-	while (n < length_2 && i < (length_1 + n))
+	while (n < len2 && i < (len1 + n))
 		new_str[i++] = s2[j++];
 
-	while (n >= length_2 && i < (length_1 + length_2))
+	while (n >= len2 && i < (len1 + len2))
 		new_str[i++] = s2[j++];
 
 	new_str[i] = '\0';
