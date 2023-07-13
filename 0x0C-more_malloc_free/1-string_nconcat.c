3dc10521ec21ae9bebe38284e9ec42d7321ce8af
raim <raim.tobi@gmail.com>
2023-07-13 15:17:23 -0700

    added 1-string_nconcat.c script

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - Concatenates two strings..
+ *
+ * @s1: first string to be displated after concatenation.
+ * @s2: secpnd string to be displayed after concatenation.
+ * @n: number ofbytes of strig s@ to be concatenated.
+ *
+ * Return: NULL - (failure); otherwise, return a
+ *	pointer to te newly allocated space in memory.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int length_1 = 0, length_2 = 0, i = 0, j = 0;
+	char *new_str;
+
+	while (s1 && s1[length_1])
+		length_1++;
+	while (s2 && s2[length_2])
+		length_2++;
+
+	if (n < length2)
+		new_str = malloc(sizeof(char) * (length_1 + n + 1));
+	else
+		new_str = malloc(sizeof(char) * (length_1 + length_2 + 1));
+
+	if (!new_str)
+		return (NULL);
+
+	while (i < length_1)
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+
+	while (n < length_2 && i < (length_1 + n))
+		new_str[i++] = s2[j++];
+
+	while (n >= length_2 && i < (length_1 + length_2))
+		new_str[i++] = s2[j++];
+
+	new_str[i] = '\0';
+
+	return (new_str);
+}
+
