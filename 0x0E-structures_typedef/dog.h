d7cc4a466825bd708c344aeb7450c4003114ebb1
raim <raim.tobi@gmail.com>
2023-07-17 15:51:57 -0700

    added dog.h script

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define  DOG_H
+
+/**
+ * struct dog - structure variable type with dog info.
+ *
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog owner.
+ *
+ * Description: Creates a structure of dog properties.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
