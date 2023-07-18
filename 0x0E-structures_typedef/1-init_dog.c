72aeb838a6d69e7f8d3b71c0d64aa35a072ad834
raim <raim.tobi@gmail.com>
2023-07-17 17:46:43 -0700

    updated 1-init_dog.v

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,6 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
+
+	/* accessing the struct elements using the '->' symbol */
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
