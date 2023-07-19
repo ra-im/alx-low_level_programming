8bbe20f9dd97f5df254224c8b2ec7a6c7d235a24
raim <raim.tobi@gmail.com>
2023-07-19 14:19:32 -0700

    added 0-print_name.c script

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ *
+ * @name: name to be printed.
+ * @f: pointer to the print function.
+ *
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+
+	f(name);
+}
+
