41a66fad6767fb6714dfd08b5155cd4cd7d2c8d0
raim <raim.tobi@gmail.com>
2023-07-19 15:44:11 -0700

    added 3-op_functions.c script

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers.
+ *
+ * @a: integer 1.
+ * @b: integer 2.
+ *
+ * Return: sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - determines the differene betweeen two integers.
+ *
+ * @a: interger 1.
+ * @b: integer 2.
+ *
+ * Return: difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - determines the product tof two integers.
+ *
+ * @a: integer 1.
+ * @b: integer 2.
+ *
+ * Return: product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - determines the result of the division
+ *		of two integers.
+ *
+ * @a: integer 1 (numerator).
+ * @b: integer 2 (denominator).
+ *
+ * Return: result of te division of a and b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Determines the modulo of two integers.
+ *
+ * @a: integer 1 (numerator).
+ * @b: integer 2 (denominator).
+ *
+ * Return: remainder of a divided by b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a % b);
+}
+
