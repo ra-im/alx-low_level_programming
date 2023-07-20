661bbd8e197b6781469b64b1f5dcfe22b31f226c
raim <raim.tobi@gmail.com>
2023-07-20 16:02:22 -0700

    added 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", va_arg(str_params, char *));
 
-		if (i != (n - 1) && separator != NULL)
+		if (i < (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
