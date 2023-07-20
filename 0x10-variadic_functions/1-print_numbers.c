ca1f04fd8ffbfba7eddcd9973a5a23144c7f2e14
raim <raim.tobi@gmail.com>
2023-07-20 15:43:47 -0700

    updatd 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,19 +11,23 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	/* creating va_list to store the variable argument list */
 	va_list int_params;
 	unsigned int i;
 
+	/* initializing valist for n number of arguments */
 	va_start(int_params, n);
 
+	/* loop through va_list n times */
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(int_params, unsigned int));
 
 		if (i < (n - 1) && separator != NULL)
-			printf("%c", *separator);
+			printf("%s", separator);
 	}
 
+	/* free va_list */
 	va_end(int_params);
 
 	printf("\n");
