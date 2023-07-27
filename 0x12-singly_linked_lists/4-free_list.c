5392db516eff135092f3e796ef488e7d76d3b91a
raim.io <raim.tobi@gmail.com>
2023-07-27 16:36:52 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ *
+ * @head: pointer to the beinning of the list_t list.
+ *
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+
+		head = temp;
+	}
+}
+
