02c4767c8f3ff5602071d08583a055e498268eb7
raim-io <raim.tobi@gmail.com>
2023-12-23 20:15:20 +0100

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * flip_bits - returns the number of bits you would need to flip 
+ * flip_bits - returns the number of bits you would need to flip
  * to get from one number to another.
  * @n: number flipping from.
  * @m: number flipping to.
