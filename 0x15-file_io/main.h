a19fb351dadf0f52a66170ae77ae11ba5af5e027
raim-io <raim.tobi@gmail.com>
2023-12-25 16:17:19 +0100

    update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,9 +5,9 @@
 #include <sys/stat.h>
 
 #include <stdlib.h>
-#include <stddef.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stddef.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
