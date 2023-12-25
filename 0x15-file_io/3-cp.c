1cb5d109a25ebe57b77b238df2da48ebb97e0e47
raim-io <raim.tobi@gmail.com>
2023-12-25 16:22:01 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -65,8 +65,7 @@ int main(int argc, char *argv[])
 	rd = read(fd_from, buffer, 1024);
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	do
-	{
+	do {
 		if (fd_from == -1 || rd == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
