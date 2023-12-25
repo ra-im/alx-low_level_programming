a123468ff1ee670edc89b44108e27d6be936d22a
raim-io <raim.tobi@gmail.com>
2023-12-25 14:06:22 +0100

    Updated 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,11 @@ int create_file(const char *filename, char *text_content)
 	int op, wr, len;
 
 	len = 0;
-	for (len; text_content[len]; len++)
-		;
+	if (text_content != NULL)
+	{
+		for (len; text_content[len]; len++)
+			;
+	}
 
 	if (filename == NULL)
 		return (-1);
