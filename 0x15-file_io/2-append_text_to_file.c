8ceb72116e0645116bd7ad77a79b4f2ea1faf11f
raim-io <raim.tobi@gmail.com>
2023-12-25 14:27:58 +0100

    update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	len = 0;
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
