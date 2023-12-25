978802a179fec2851ae75943838494e46a294a2a
raim-io <raim.tobi@gmail.com>
2023-12-25 13:55:11 +0100

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: name of file to be created
+ * @text_content: null terminated string => file content.
+ * Return: 1 success, -1 failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int op, wr, len;
+
+	len = sizeof(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
 #include <stddef.h>
 #include <fcntl.h>
 #include <unistd.h>
