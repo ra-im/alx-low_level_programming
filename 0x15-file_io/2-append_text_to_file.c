144d24cde1e1212bfaef205829d3943be4dba92a
raim-io <raim.tobi@gmail.com>
2023-12-25 14:25:12 +0100

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to file
+ * @filename: name of the file
+ * @text_content: null terminated string added
+ * at the end of the file
+ * Return: 1 success, -1 failure
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op, wr, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+	}
+
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+	return (1);
+}
