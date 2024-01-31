c92da3d37d4fbaffb55cd34a520e1f9014a63794
Raheem Oluwatobiloba <raim.tobbi@gmail.com>
2024-01-31 15:12:59 +0100

    feat: insert new node at any given positon in a doubly linked list + test

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert a new node at
+ * a given position
+ *
+ * @h: list head
+ * @idx: index provided
+ * @n: new node data
+ *
+ * Return: address of the new node, or NULL on failure
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i;
+	dlistint_t *new_node, *head = *h;
+
+	new_node = NULL;
+
+	if (idx == 0)
+		new_node = add_dnodeint(h, n);
+	else
+	{
+		i = 1; if (head != NULL) while (head->prev != NULL)
+				head = head->prev;
+
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->prev = head;
+						new_node->next = head->next;
+						head->next->prev = new_node;
+						head->next = new_node;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
+	return (new_node);
+}
