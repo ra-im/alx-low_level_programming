f5dc057bd18ff3241b339869f2023693156bdc9a
Raheem Oluwatobiloba <raim.tobbi@gmail.com>
2024-01-31 16:45:51 +0100

    fix: generate kegen for crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,51 +1,127 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * generate_key - generates a key gen based on username
+ * f4 - finds the biggest number
  *
- * @username: username
- * @key: key
- *
- * Return: void
-*/
-void generate_key(const char *username, char *key)
+ * @usrn: username
+ * @len: length of username
+ * Return: the biggest number
+ */
+int f4(char *usrn, int len)
 {
-	int len = strlen(username);
-	int i;
+	int ch;
+	int vch;
+	unsigned int rand_num;
 
-	/*Simple key generation algorithm (example)*/
-	for (i = 0; i < len; i++)
+	ch = *usrn;
+	vch = 0;
+
+	while (vch < len)
 	{
-		key[i] = username[i] + 1; /*Modify this line based on the actual algorithm*/
+		if (ch < usrn[vch])
+			ch = usrn[vch];
+		vch += 1;
 	}
 
-	key[len] = '\0';
+	srand(ch ^ 14);
+	rand_num = rand();
+
+	return (rand_num & 63);
+}
+
+/**
+ * f5 - multiplies each char of username
+ *
+ * @usrn: username
+ * @len: length of username
+ * Return: multiplied char
+ */
+int f5(char *usrn, int len)
+{
+	int ch;
+	int vch;
+
+	ch = vch = 0;
+
+	while (vch < len)
+	{
+		ch = ch + usrn[vch] * usrn[vch];
+		vch += 1;
+	}
+
+	return (((unsigned int)ch ^ 239) & 63);
+}
+
+/**
+ * f6 - generates a random char
+ *
+ * @usrn: username
+ * Return: a random char
+ */
+int f6(char *usrn)
+{
+	int ch;
+	int vch;
+
+	ch = vch = 0;
+
+	while (vch < *usrn)
+	{
+		ch = rand();
+		vch += 1;
+	}
+
+	return (((unsigned int)ch ^ 229) & 63);
 }
 
 /**
  * main - Entry point
  *
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 1 success, 0 otherwise
-*/
-int main(int argc, char *argv[])
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
 {
-	if (argc != 2)
+	char keygen[7];
+	int len, ch, vch;
+	long alph[] = {
+			0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
+			0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
+			0x723161513346655a, 0x6b756f494b646850};
+	(void)argc;
+
+	for (len = 0; argv[1][len]; len++)
+		;
+	/* ----------- f1 ----------- */
+	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
+	/* ----------- f2 ----------- */
+	ch = vch = 0;
+	while (vch < len)
 	{
-		fprintf(stderr, "Usage: %s <username>\n", argv[0]);
-		return (1);
+		ch = ch + argv[1][vch];
+		vch = vch + 1;
 	}
-
-	char key[256];
-
-	/*Generate key based on the provided username*/
-	generate_key(argv[1], key);
-
-	/*Print the generated key*/
-	printf("%s\n", key);
-
+	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
+	/* ----------- f3 ----------- */
+	ch = 1;
+	vch = 0;
+	while (vch < len)
+	{
+		ch = argv[1][vch] * ch;
+		vch = vch + 1;
+	}
+	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
+	/* ----------- f4 ----------- */
+	keygen[3] = ((char *)alph)[f4(argv[1], len)];
+	/* ----------- f5 ----------- */
+	keygen[4] = ((char *)alph)[f5(argv[1], len)];
+	/* ----------- f6 ----------- */
+	keygen[5] = ((char *)alph)[f6(argv[1])];
+	keygen[6] = '\0';
+	for (ch = 0; keygen[ch]; ch++)
+		printf("%c", keygen[ch]);
 	return (0);
 }
