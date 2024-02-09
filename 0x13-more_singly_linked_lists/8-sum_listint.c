c266d84c12019356ac00e0662d11127afb64ea55
Raheem Oluwatobiloba <raim.tobbi@gmail.com>
2024-02-09 08:44:06 +0100

    feat: return the sum of all node data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums up all the data(n) of a listint_t singly linked list
+ *
+ * @head: list head
+ *
+ * Return: sum, otherwise 0 if empty
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
