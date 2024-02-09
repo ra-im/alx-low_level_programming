9e8a2e237126d07c76589a5a924446961ccacee1
Raheem Oluwatobiloba <raim.tobbi@gmail.com>
2024-02-09 08:10:35 +0100

    fix: free a non null sinngly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,16 +10,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t **temp;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		*temp = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
-		*head = *temp;
+		*head = temp;
 	}
 
 	*head = NULL;
