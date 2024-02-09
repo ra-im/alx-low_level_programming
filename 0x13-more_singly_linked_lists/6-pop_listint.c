1dedc33ad300f99d4e1826b3c93f00deffce0fa1
Raheem Oluwatobiloba <raim.tobbi@gmail.com>
2024-02-09 08:20:42 +0100

    feat: delete the head node of a singly linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a singly linked list
+ *
+ * @head: head of the list
+ *
+ * Return: head node data, other wise 0 if empty
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int node_data;
+
+	while (*head || head)
+	{
+		node_data = (*head)->n;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+
+		return (node_data);
+	}
+
+	return (0);
+}
