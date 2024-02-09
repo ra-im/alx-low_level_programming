913af0e00cd6f7c8b4cee6015d6b732c61915758
Raheem Oluwatobiloba <raim.tobbi@gmail.com>
2024-02-09 08:03:49 +0100

    freat: free a non null sinngly linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t singly linked list
+ *
+ * @head: head of the list
+ *
+ * Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t **temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		*temp = (*head)->next;
+		free(*head);
+		*head = *temp;
+	}
+
+	*head = NULL;
+}
