f1c41e6b1c4273be41aa40346274b3e4388e9f48
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-07-18 01:31:40 +0100

    feat: jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,10 +5,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 /* STRUCTURES */
 
